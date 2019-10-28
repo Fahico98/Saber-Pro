7e09946825dbdc7387da6ac35cbbe7929373f9f6
Fahico <fahico98@gmail.com>
2019-10-28 12:33:00 -0500

    Commit_12

--- database/migrations/2019_10_23_015750_define_foreign_keys.php
+++ database/migrations/2019_10_23_015750_define_foreign_keys.php
@@ -14,35 +14,35 @@ class DefineForeignKeys extends Migration{
     public function up(){
 
         Schema::table('users', function(Blueprint $table){
-            $table->foreign('role_id')->references('id')->on('roles');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
         });
 
         Schema::table('programa', function(Blueprint $table){
-            $table->foreign('facultad_id')->references('id')->on('facultad');
+            $table->foreign('facultad_id')->references('id')->on('facultad')->onDelete('cascade');
         });
 
         Schema::table('competencia', function(Blueprint $table){
-            $table->foreign('asignatura_id')->references('id')->on('asignatura');
+            $table->foreign('asignatura_id')->references('id')->on('asignatura')->onDelete('cascade');
         });
 
         Schema::table('asignatura', function(Blueprint $table){
-            $table->foreign('programa_id')->references('id')->on('programa');
+            $table->foreign('programa_id')->references('id')->on('programa')->onDelete('cascade');
         });
 
         Schema::table('resultado', function(Blueprint $table){
-            $table->foreign('asignatura_id')->references('id')->on('asignatura');
+            $table->foreign('asignatura_id')->references('id')->on('asignatura')->onDelete('cascade');
         });
 
         Schema::table('criterio', function(Blueprint $table){
-            $table->foreign('resultado_id')->references('id')->on('resultado');
+            $table->foreign('resultado_id')->references('id')->on('resultado')->onDelete('cascade');
         });
 
         Schema::table('afirmacion', function(Blueprint $table){
-            $table->foreign('modulo_id')->references('id')->on('modulo');
+            $table->foreign('modulo_id')->references('id')->on('modulo')->onDelete('cascade');
         });
 
         Schema::table('evidencia', function(Blueprint $table){
-            $table->foreign('afirmacion_id')->references('id')->on('afirmacion');
+            $table->foreign('afirmacion_id')->references('id')->on('afirmacion')->onDelete('cascade');
         });
     }
 
